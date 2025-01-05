82aa39a19a6fdb1faa1f888da9415ee5426814ea
xor <mrkeule@gmail.com>
2025-01-05 23:56:02 +0100

    NO FIX YET: test for faulty serialization of multiple references to the same list

--- test_js3.py
+++ test_js3.py
@@ -1,12 +1,10 @@
 import datetime
 import os
-import shutil
 from pathlib import Path
-from typing import Optional, List, Dict
-from unittest import TestCase
-
 from shared.js3 import JS3, JS3Enc
 from shared.js3dec import JS3Dec
+from typing import Optional, List, Dict, Any
+from unittest import TestCase
 
 
 class DummyDate(JS3):
@@ -20,6 +18,8 @@ class Dummy(JS3):
         self.name: str = "name not set"
         self.related: Optional[Dummy] = None
         self.related_ls: List[Dummy] = []
+        self.any_list_1: Optional[List[Any]] = None
+        self.any_list_2: Optional[List[Any]] = None
 
 
 class TestJS3Enc(TestCase):
@@ -75,3 +75,10 @@ class TestJS3Enc(TestCase):
         self.assertEqual(a.related_ls[1], b)
         self.assertEqual(b.related_ls[0], a)
         self.assertEqual(b.related_ls[1], b)
+
+    def test_ref_ls2(self):
+        self.dummy_a.any_list_1 = [1, 2, 3]
+        self.dummy_a.any_list_2 = self.dummy_a.any_list_1
+        JS3Enc(self.dummy_a).save(self.js, indent=2)
+        d: Dummy = JS3Dec().source(self.js).decode()
+        self.assertEqual(d.any_list_1, self.dummy_a.any_list_1)
